35be3332de51553ebd0f03814e77ba04279d025c
Von Diesel & iman mafi <student@epicodus.com>
2015-09-15 09:44:43 -0700

    initial commit

---
+++ spec/specs.js
@@ -0,0 +1,27 @@
+describe('Contact', function() {
+  it('creates a new contact with the given specs', function() {
+    var testContact = new Contact("Dunning", "Kruger");
+    expect(testContact.firstName).to.equal("Dunning");
+    expect(testContact.lastName).to.equal("Kruger");
+    expect(testContact.addresses).to.eql([]);
+  });
+
+  it('add the fullName method to all contacts', function(){
+    var testContact = new Contact("Sherlock", "Holmes");
+    expect(testContact.fullName()).to.equal("Sherlock Holmes");
+  });
+});
+
+describe('Address', function() {
+  it('creates a new address with the given specs', function() {
+    var testAddress = new Address("123 Sesame Street", "New York", "NY");
+    expect(testAddress.street).to.equal("123 Sesame Street");
+    expect(testAddress.city).to.equal("New York");
+    expect(testAddress.state).to.equal("NY");
+  });
+
+  it('adds the fullAddress method to all addresses', function() {
+    var testAddress = new Address("123 Sesame Street", "New York", "NY");
+    expect(testAddress.fullAddress()).to.equal("123 Sesame Street, New York, NY");
+  });
+});
